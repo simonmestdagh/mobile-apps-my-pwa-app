955d23d6838d528df0a4a2b874393084a593deb6
simon mestdagh <simonmestdagh2001@gmail.com>
2021-12-06 12:17:28 +0100

    qsfdjk

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,12 @@ export default {
   created() {
     document.addEventListener("swupdatefound", this.updateTheApp, {
       once: true,
-    });,
-     navigator.serviceWorker.addEventListener(
- 'controllerchange', () => {
- if (this.isRefreshing) return;
- this.isRefreshing = true;
- window.location.reload();
- }
- );
+    });
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (this.isRefreshing) return;
+      this.isRefreshing = true;
+      window.location.reload();
+    });
   },
   data: function () {
     return {
